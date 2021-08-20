beb2fabdcdffdd8707acc7d86565a5b78fc42794
Mr.Hope <zhangbowang1998@gmail.com>
2021-08-20 18:26:35 +0800

    chore: update page

--- scripts/push.ts
+++ scripts/push.ts
@@ -22,20 +22,23 @@ export const pushPages = (): Promise<void> => {
   const promises = appidList.map((appid) => {
     if (appid === "1109559721") return Promise.resolve();
 
-    return axios
-      .get<{ access_token: string }>(
-        `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appIDInfo[appid]}`
-      )
-      .then(({ data }) =>
-        axios.post(
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          `https://api.weixin.qq.com/wxa/search/wxaapi_submitpages?access_token=${data.access_token}`,
-          { pages: pageLists }
+    return (
+      axios
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        .get<{ access_token: string }>(
+          `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appIDInfo[appid]}`
         )
-      )
-      .then(({ data }) => {
-        console.log(data);
-      });
+        .then(({ data }) =>
+          axios.post(
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            `https://api.weixin.qq.com/wxa/search/wxaapi_submitpages?access_token=${data.access_token}`,
+            { pages: pageLists }
+          )
+        )
+        .then(({ data }) => {
+          console.log(data);
+        })
+    );
   });
 
   return Promise.all(promises).then(() => {
