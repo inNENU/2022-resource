d039f7b68b8a23103cccb12add3d25d55bc8928c
Mr.Hope <zhangbowang1998@gmail.com>
2021-09-03 23:28:46 +0800

    feat: update wechat

--- scripts/gen/wechat.ts
+++ scripts/gen/wechat.ts
@@ -1,5 +1,6 @@
-// import { WechatDetail } from "../../typings";
-// import axios from "axios";
+import axios from "axios";
+import { readFileSync, writeFileSync } from "fs";
+import { getFileList } from "../util";
 
 export interface WechatConfig {
   /** 公众号名称 */
@@ -25,11 +26,46 @@ export interface WechatConfig {
   }[];
 }
 
-// export const genWechat = (data: WechatConfig): Promise<WechatDetail> => {
-//   const promises = [];
-//   data.content.forEach((item) => {
-//     if (!item.cover || !item.title) axios();
-//   });
+export const genWechat = (filePath: string): Promise<void> => {
+  let content = readFileSync(`./res/function/wechat/${filePath}`, {
+    encoding: "utf-8",
+  });
 
-//   return data;
-// };
+  const results = content
+    .split("\n")
+    .map((item) => (/- url: (.*)$/.exec(item) || [])[1] || "")
+    .filter((item) => item.length);
+
+  return Promise.all(
+    results.map((item) =>
+      axios.get(item).then(({ data }) => {
+        const [, cover = ""] =
+          /<meta property="og:image" content="(.*?)" \/>/.exec(data) || [];
+        const [, title = ""] =
+          /<meta property="og:title" content="(.*?)" \/>/.exec(data) || [];
+        const [, desc = ""] =
+          /<meta property="og:description" content="(.*?)" \/>/.exec(data) ||
+          [];
+
+        content = content.replace(
+          `- url: ${item}`,
+          `- cover: ${cover}\n    title: ${title}\n${
+            desc ? `    desc: ${desc}\n` : ""
+          }    url: ${item}`
+        );
+      })
+    )
+  ).then(() => {
+    writeFileSync(`./res/function/wechat/${filePath}`, content, {
+      encoding: "utf-8",
+    });
+  });
+};
+
+const fileList = getFileList("./res/function/wechat", "yml");
+
+fileList
+  .filter((item) => item !== "index.yml")
+  .forEach((item) => {
+    genWechat(item);
+  });
